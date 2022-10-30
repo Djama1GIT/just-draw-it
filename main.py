eeca2f311289d33dd58b26f607be4064f28b337d
Dj.ama1 <dj.ama1@mail.ru>
2022-10-30 10:05:24 +0300

    re v0.1

--- main.py
+++ main.py
@@ -5,11 +5,37 @@ with open('token.txt') as _token:
     token = _token.readline()
 bot = telebot.TeleBot(token)
 
+default_paper_size = (40, 34)
+px_in_px = 40
+threshold = 100
+
+
+class Img:
+    def __init__(self, image_src=None):
+        self.image_src = image_src
+        self.image = Image.open('pic.jpg')
+
+    def re(self):
+        self.image = self.image.resize(default_paper_size, resample=Image.NONE)
+        for left in range(self.image.size[0]):
+            for up in range(self.image.size[1]):
+                self.image.putpixel((left, up),
+                                    (0, 0, 0) if sum(self.image.getpixel((left, up))) // 3 < threshold else (255, 255, 255))
+        self.image = self.image.resize((default_paper_size[0] * px_in_px, default_paper_size[1] * px_in_px),
+                                       resample=Image.NONE)
+        self.image.save('out.jpg')
+        self.image.show()
+
+
+x = Img()
+x.re()
+
 
 @bot.message_handler(commands=['start'])
 def _start(message):
     msg = """
-    Скорее отправляй фото!\nХочешь узнать как будет происходить процесс рисования? Используй команду /help
+    Скорее отправляй фото!
+Хочешь узнать как будет происходить процесс рисования? Используй команду /help
     """
     bot.send_message(message.chat.id, msg)
 
@@ -17,7 +43,8 @@ def _start(message):
 @bot.message_handler(commands=['help'])
 def _help(message):
     msg = """
-    123
+    Итак. Всё на самом деле просто!
+Ты отправляешь мне фото - я предлагаю варианты рисования - ты выбираешь понравившийся и мы вместе рисуем! Готово!
     """
     bot.send_message(message.chat.id, msg)
 
