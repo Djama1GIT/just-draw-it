96a1206a2d8acb23fc128dd5af82ecac29e6bf36
Dj.ama1 <dj.ama1@mail.ru>
2023-01-16 02:37:15 +0300

    db

--- db.py
+++ db.py
@@ -4,6 +4,14 @@ import sqlite3
 try:
     con = sqlite3.connect('chats.db')
     cursor = con.cursor()
+    create_query = '''CREATE TABLE chats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    method INTEGER NOT NULL,
+    step INTEGER NOT NULL,
+    sizes VARCHAR(10) NOT NULL,
+    image TEXT NOT NULL
+    );'''
+    cursor.execute(create_query)
 except Exception as e:
     with open('log.txt', 'a') as log:
         err = f'[{datetime.datetime.now()}] DB Error {e} | Arguments: {e.args}\n'
